e669ab062eb0c5cfc8332d6deb014f37ef3e4435
1 <masmirnov.inf@gmail.com>
2017-06-22 19:22:54 +0300

    merged 2 branches

--- app/src/main/java/com/example/myapplication/MainMenu.java
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -4,15 +4,11 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
-
 public class MainMenu extends AppCompatActivity {
-
-    String debug = "DEBUG";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
